3ec94d93e3f15723e94ed6d18bb382c8e03073cb
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-01-31 18:29:51 +0530

    forms added

---
+++ analyser/PrivacyAnalyser/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class policy(forms.Form):
+    policy_text = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'id':'exampleFormControlTextarea1'}))
+
