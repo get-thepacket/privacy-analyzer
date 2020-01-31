8eed819ef49b8e41d934a874f4a689cca0f04084
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-01-31 18:28:25 +0530

    form processing

--- analyser/PrivacyAnalyser/views.py
+++ analyser/PrivacyAnalyser/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import policy
+
 
 def index(request):
-    return render(request,'index.html')
+
+    if request.method == 'POST':
+
+        # process form data
+        form = policy(request.POST)
+        if form.is_valid():
+            policy_text = form.cleaned_data['policy_text']
+            print(policy_text)
+
+    return render(request,'index.html', {'form': policy()})
